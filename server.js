2fc9d6f6670f2f69acdb4d85b993f3500827451c
Sridhar <srilumia@hotmail.com>
2024-10-08 13:16:42 +0530

    basic nodejs setup

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+//create a database connection
+mongoose
+  .connect(
+    "mongodb+srv://ecomadmin:h9Fh5VINgNjIEuXU@cluster0.sgxhr.mongodb.net/"
+  )
+  .then(() => console.log("MongoDB Connection Established"))
+  .catch((err) => console.log(err));
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is now running on PORT ${PORT}`));
